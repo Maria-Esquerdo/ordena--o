1b9212ba9dd56047623436ccb6dbead214de789f
Maria-Esquerdo <esquerdocunhamariaclara@gmail.com>
2026-06-01 03:16:41 -0300

    :tada: inicia repositório - algoritmos de ordenação

---
+++ bubble_sort.cpp
@@ -0,0 +1,12 @@
+// Algoritmo de ordenação Bubble Sort
+#include <iostream>
+using namespace std;
+
+//Compara os elementos de 2 em 2, identificando o menor entre eles e colocando na posição certa
+// Isso se repete sucessivamente até o vetor estar ordenado
+
+void bubble_sort (int vet[], int n)
+{
+    
+
+}
