be16967ab934eb430e7848cde4f94c7ae2ea3df4
Maria-Esquerdo <esquerdocunhamariaclara@gmail.com>
2026-06-03 12:36:12 -0300

    :memo: quick sort corrigido e testado

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -2,12 +2,11 @@
 using namespace std;
 
 // Algoritmo de  ordenação Quick Sort:
-// 1. Função de partição: 
-    //1.1 -> escolhe um pivô e passa ele pra primeira posição
-    //1.2 -> compara o pivô com o resto do vetor, se um elemento for menor, a troca é feita e o contador é
-    //atualizado para acompanhar o elemento do pivô
-    //1.3 -> volta o pivô para sua posição original 
-
+// 1. Função de partição:
+// 1.1 -> escolhe um pivô e passa ele pra primeira posição
+// 1.2 -> compara o pivô com o resto do vetor, se um elemento for menor, a troca é feita e o contador é
+// atualizado para acompanhar o elemento do pivô
+// 1.3 -> volta o pivô para sua posição original
 
 void imprime(int vet[], int n)
 {
@@ -17,7 +16,7 @@ void imprime(int vet[], int n)
     }
 }
 
-void troca (int *a, int *b)
+void troca(int *a, int *b)
 {
     int aux = *a;
     *a = *b;
@@ -26,31 +25,51 @@ void troca (int *a, int *b)
 }
 
 // Função de partição e ordenação a partir do pivô:
-void partition (int vet[], int esq, int dir)
+int partition(int vet[], int esq, int dir)
 {
-    int pivo = (esq+dir) / 2;
+    int pivo = (esq + dir) / 2;
     int i = esq;
-    troca(&pivo, &i);
-    
-    
-
-
-    return i;
+    troca(&vet[pivo], &vet[esq]);
+    // Percorrendo o vetor, da segunda posição, até o final, comparando o elemento atual (j) com o pivô
+    // e trocando o elemento da frente do pivô (i) em diante, se for menor
+    for (int j = esq + 1; j <= dir; j++)
+    {
+        if (vet[j] < vet[esq]) // compara elemento atual com pivô
+        {
+            i++;                     // Se satisfizer à condição, adiciona 1 ao contador de início e o elemento j (atual)
+            troca(&vet[i], &vet[j]); ////é trocado com o elemento mais à esquerda (sem ser o primeiro, que é o pivô)
+        }
+    }
+    // por fim, o contador i aponta para o último elemento do vetor que é maior que o pivô
+    // então o pivô, que está no começo, é trocado com i
+    troca(&vet[esq], &vet[i]);
+    return i; // retorna a posição do pivô original
 }
 
-//Funções para dividir o vetor, através dos pivôs, recursivamente
-void aux_quick_sort (int vet[], int esq, int dir)
+// Funções para dividir o vetor, através dos pivôs, recursivamente
+void aux_quick_sort(int vet[], int esq, int dir)
 {
-    
-
+    if (esq >= dir)
+        return;
+    else
+    {
+        int pi; 
+        pi = partition(vet, esq, dir); //Chama a função para os valores iniciais, definindo pi como 
+        //a posição do pivô (local de partição)
+        //A ideia agora é partir o vetor á direita e a esquerda do pivô, excluindo ele, já que está ordenado.
+        // da mesma forma, até chegar em vetores de tam 1
+        aux_quick_sort(vet, esq, pi-1); //p1-1 é o índice do elemento anterior ao vetor
+        aux_quick_sort(vet, pi+1, dir); //pi+1 é o elemento à frente do pivô
+        //aqui, excluímos o pivõ da chamada recursiva. ordenamos o vetor de elementos menores que o pivô e o vetor 
+        //de elementos maiores que o pivô
+    }
+    return;
 }
 
-void quick_sort (int vet[], int n)
+void quick_sort(int vet[], int n)
 {
-
-
+    aux_quick_sort(vet, 0, n-1);
 }
-
 
 int main()
 {
